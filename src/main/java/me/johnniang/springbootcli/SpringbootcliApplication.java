3ea03ea9ee0ff9023f3bbea4b8e8466330c8a0c3
johnniang <johnniang@riseup.net>
2019-04-10 21:44:34 +0800

    first commit

---
+++ src/main/java/me/johnniang/springbootcli/SpringbootcliApplication.java
@@ -0,0 +1,13 @@
+package me.johnniang.springbootcli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootcliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootcliApplication.class, args);
+	}
+
+}
